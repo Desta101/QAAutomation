7699ba5d99e9d4bd9dd40094ef1c2f4ca3696099
Shimon Desta <34807256+Desta101@users.noreply.github.com>
2024-08-14 16:55:35 +0300

    Update userLogin_test.ts

--- userLogin_test.ts
+++ userLogin_test.ts
@@ -1,6 +1,6 @@
 Feature('User Login');
 
-import { email,navigateToLoginPage } from './commonSetup';
+import {email, navigateToLoginPage, password} from './commonSetup';
 
 Scenario('Navigate to the login page and verify elements', ({ I }) => {
     navigateToLoginPage(I);
@@ -10,10 +10,10 @@ Scenario('Navigate to the login page and verify elements', ({ I }) => {
 Scenario('Login with valid credentials', ({ I }) => {
     navigateToLoginPage(I);
     I.waitForElement('#email', 5);
-    I.fillField('#email', email); 
-    I.fillField('#password', '12345678'); 
+    I.fillField('#email', email);
+    I.fillField('#password', '12345678');
     I.click('#submit');
-    I.seeInCurrentUrl('/contactList'); 
+    I.seeInCurrentUrl('/contactList');
 });
 
 Scenario('Prevent login with invalid credentials', ({ I }) => {
@@ -22,7 +22,32 @@ Scenario('Prevent login with invalid credentials', ({ I }) => {
     I.fillField('#email', email);
     I.fillField('#password', 'wrongPassword');
     I.click('#submit');
-    I.waitForElement('#error', 5); 
-    I.see('Incorrect username or password', '#error'); 
+    I.waitForElement('#error', 5);
+    I.see('Incorrect username or password', '#error');
+});
+
+Scenario('should fail login with incorrect password', ({ I }) => {
+    navigateToLoginPage(I);
+    I.fillField('#email', 'validuser@example.com');
+    I.fillField('#password', 'wrongpassword');
+    I.click('#submit');
+    I.see('Incorrect username or password', '#error');
+});
+
+Scenario('should fail login with incorrect username', ({ I }) => {
+    navigateToLoginPage(I);
+    I.fillField('#email', 'wronguser@example.com');
+    I.fillField('#password', 'correctpassword');
+    I.click('#submit');
+    I.see('Incorrect username or password', '#error');
+});
+
+Scenario('should login successfully with correct credentials', ({ I }) => {
+    navigateToLoginPage(I);
+    I.waitForElement('#email', 5);
+    I.fillField('#email', email); // וודא שכתובת האימייל והסיסמה נכונים
+    I.fillField('#password', password);
+    I.click('#submit');
+    I.seeInCurrentUrl('/contactList'); // זה מה שנצפה לראות אם ההתחברות מצליחה
 });
 
