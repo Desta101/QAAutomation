571f75ad5ee24af94ae450eb911b4dffbb157bdb
Shimon Desta <34807256+Desta101@users.noreply.github.com>
2024-08-14 09:50:29 +0300

    Update userRegistration_test.ts

--- userRegistration_test.ts
+++ userRegistration_test.ts
@@ -10,7 +10,7 @@ Scenario('Handle duplicate email registration', ({ I }) => {
     navigateToSignUpPage(I);
     I.fillField('#firstName', 'Shimon');
     I.fillField('#lastName', 'Desta');
-    I.fillField('#email', 'ShimonDesta2@gmail.com');
+    I.fillField('#email', email);
     I.fillField('#password', 'securePass');
     I.click('#submit');
     I.see('Email address is already in use', '#error');
